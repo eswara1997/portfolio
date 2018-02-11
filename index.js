c42d8fa6db663e14b87931d472de433919a19771
eswar <eswara@mit.edu>
2018-02-10 19:21:20 -0500

    first

---
+++ index.js
@@ -0,0 +1,17 @@
+new TypeIt('h1', {
+  //lifelike: true,
+  nextStringDelay: 400,
+  deleteSpeed: 100
+
+})
+  .type(' <u style="text-decoration-color: #66fcf1;"> VR and AR.</u>')
+.pause(500)
+.delete(10)
+.type('<u style="text-decoration-color: #66fcf1;">creative design.</u>')
+.pause(500)
+.delete(16)
+.type('<u style="text-decoration-color: #66fcf1 #4717f6;">new technologies.</u>')
+.pause(500)
+.delete(2)
+.type('<u style="text-decoration-color: #66fcf1;">building things!</u>')
+.pause(500);
